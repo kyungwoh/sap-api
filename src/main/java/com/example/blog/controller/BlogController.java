66297faa7f9c3a1ba55bcf47e837c199eb6f9ad9
[Kenny] Kyungwoo Hyun <kyungwoo.hyun@nhnglobal.com>
2020-12-09 02:15:16 -0800

    port 5000, JVM 1.8, log.info()

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -4,26 +4,42 @@ import com.example.blog.model.response.CategoryResponse;
 import com.example.blog.model.response.HeadlineResponse;
 import com.example.blog.model.response.PostResponse;
 import com.example.blog.service.BlogService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class BlogController {
     private final BlogService blogService;
     public BlogController(BlogService blogService) {
+        log.info("BlogController contructor");
         this.blogService = blogService;
+    }
+
+    @GetMapping("")
+    public String getHome() {
+        log.info("getHome()");
+        return "home";
     }
 
     @GetMapping("categories")
     public List<CategoryResponse> getCategories() {
+        log.info("getCategories()");
         return blogService.getCategories();
     }
 
     @GetMapping("posts")
-    public List<PostResponse> getPosts() { return blogService.getPosts(); }
+    public List<PostResponse> getPosts() {
+        log.info("getPosts()");
+        return blogService.getPosts();
+    }
 
     @GetMapping("headlines")
-    public List<HeadlineResponse> getHeadlines() { return blogService.getHeadlines(); }
+    public List<HeadlineResponse> getHeadlines() {
+        log.info("getHeadlines()");
+        return blogService.getHeadlines();
+    }
 }
